ddda8103dcd1eb1522d724f2f1be4bd5c7db9f36
Andres-14 <ajimenez.8132@unimar.edu.ve>
2025-06-29 12:38:02 -0400

    Añadi codigo a JavaClass PacientesClinica, donde trabajo con lo basico relacionado a los pacientes de la clinica en la simulacio, como su clase paciente con sus propiedades como el nombre, id o diagnostico. Ademas de trabajar tambien algunos metodos como el ToString

--- src/main/java/com/mycompany/sistemaclinica/PacientesClinica.java
+++ src/main/java/com/mycompany/sistemaclinica/PacientesClinica.java
@@ -1,13 +1,62 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.sistemaclinica;
 
-/**
- *
- * @author HP
- */
+import java.time.LocalDate;
+import java.util.Objects;
+
 public class PacientesClinica {
+ private static int sigId = 1000; // Da un ID al paciente que empieza desde el 1000
+    private int id;
+    private String nombre;
+    private String sexo;
+    private LocalDate fechaNacimiento;
+    private String diagnostico;
+
+    public PacientesClinica(String nombre, String sexo, LocalDate fechaNacimiento, String diagnostico) {
+        this.id = sigId++;
+        this.nombre = nombre;
+        this.sexo = sexo;
+        this.fechaNacimiento = fechaNacimiento;
+        this.diagnostico = diagnostico;   
+    }
     
+    public PacienteClinica(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public String getDiagnosttico() {
+        return diagnostico;
+    }
+
+    public String toString() {
+        return "ID: " + id + ", Nombre del paciente: " + nombre + ", Sexo: " + sexo +
+               ", Fecha de Nacimiento: " + fechaNacimiento + ", Diagnostico obtenido: " + diagnostico;
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PacientesClinica paciente = (PacientesClinica) o;
+        return id == paciente.id;
+    }
+
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
