40f289f35fbb3a6d395f944538d713ce8baa4e92
Andres-14 <ajimenez.8132@unimar.edu.ve>
2025-06-29 13:56:19 -0400

    Añadi el codigo a la JavaClass PacientesEncoladosClinica, que se refiere a los pacientes que estan en cola para ser atentidos, con sus propiedades como nombre, o motivoC (motivo consulta). Tambien añadi el metodo toString, entre otros

--- src/main/java/com/mycompany/sistemaclinica/PacientesEncoladosClinica.java
+++ src/main/java/com/mycompany/sistemaclinica/PacientesEncoladosClinica.java
@@ -1,13 +1,68 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.sistemaclinica;
 
-/**
- *
- * @author HP
- */
+import java.util.Objects;
+
 public class PacientesEncoladosClinica {
-    
+    private int id;
+    private String nombre;
+    private String motivoC;
+    private int prioridad; //Los numeros más cercanos al 0 son más prioritarios
+
+    public PacientesEncoladosClinica(int id, String nombre, String motivoC, int prioridad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.motivoC = motivoC;
+        this.prioridad = prioridad;
+    }
+
+    public PacientesEncoladosClinica(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getMotivoC() {
+        return motivoC;
+    }
+
+    public int getPrioridad() {
+        return prioridad;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setReason(String motivoC) {
+        this.motivoC = motivoC;
+    }
+
+    public void setPriority(int prioridad) {
+        this.prioridad = prioridad;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id + ", Nombre del paciente: " + nombre + ", Motivo de la consulta: " + motivoC + ", Prioridad: " + prioridad;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PacientesEncoladosClinica that = (PacientesEncoladosClinica) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
